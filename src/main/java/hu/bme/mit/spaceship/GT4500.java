110efe19ffab0919da46c602f2a9506b1ec48079
Fabian Erik <fabianerik95@gmail.com>
2019-05-10 09:04:25 +0200

    Implement some Unit tests

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -15,6 +15,11 @@ public class GT4500 implements SpaceShip {
     this.secondaryTorpedoStore = new TorpedoStore(10);
   }
 
+  public GT4500(TorpedoStore primaryTorpedoStore, TorpedoStore secondaryTorpedoStore) {
+    this.primaryTorpedoStore = primaryTorpedoStore;
+    this.secondaryTorpedoStore = secondaryTorpedoStore;
+  }
+
   public boolean fireLaser(FiringMode firingMode) {
     // TODO not implemented yet
     return false;
@@ -78,7 +83,6 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
         boolean primarySucces = primaryTorpedoStore.fire(1);
         boolean secondarySuccess = secondaryTorpedoStore.fire(1);
         firingSuccess = primarySucces || secondarySuccess;
